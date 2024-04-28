de604538100115ba98b21ce23832c9bd0c64cd0a
Billy Arnold <billyarnoldd@gmail.com>
2024-04-28 20:33:57 +0100

    ...

--- components/profile.tsx
+++ components/profile.tsx
@@ -7,8 +7,8 @@ interface ProfileProps {
 
 const Profile = ({ profile }: ProfileProps) => {
   return (
-    <div className="flex flex-col mb-6 w-full">
-      <h1 className="text-2xl mb-2 flex-grow">{profile.display_name}</h1>
+    <div className="flex flex-col">
+      <h1 className="text-xl flex-grow">{profile.display_name}</h1>
       <Link href={profile.uri}>View on spotify</Link>
       <p>{profile.country}</p>
     </div>
